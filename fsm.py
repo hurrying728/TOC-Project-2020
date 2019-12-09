7353805771d957dfffa2d0f66e3e7960d4979c96
F74066218 <f74066218@mail.ncku.edu.tw>
2019-12-09 17:41:40 +0800

    change state name

--- fsm.py
+++ fsm.py
@@ -12,47 +12,47 @@ class TocMachine(GraphMachine):
         text = event.message.text
         return text == "開始使用"
 
-    def is_going_to_state1(self, event):
+    def is_going_to_before_register(self, event):
         text = event.message.text
         return text == "尚未開始註冊宿網"
 
-    def is_going_to_state2(self, event):
+    def is_going_to_after_register(self, event):
         text = event.message.text
         return text == "已開始註冊宿網"
 
-    def is_going_to_state3(self, event):
+    def is_going_to_use_router(self, event):
         text = event.message.text
         return text == "使用分享器"
 
-    def is_going_to_state4(self, event):
+    def is_going_to_not_use_router(self, event):
         text = event.message.text
         return text == "無使用分享器"
 
-    def is_going_to_state5(self, event):
+    def is_going_to_already_register(self, event):
         text = event.message.text
         return text == "已註冊"
 
-    def is_going_to_state6(self, event):
+    def is_going_to_not_register(self, event):
         text = event.message.text
         return text == "未註冊" or text == "已更換至未註冊網孔"
 
-    def is_going_to_state6a(self, event):
+    def is_going_to_check_url(self, event):
         text = event.message.text
         return text == "仍無法連線"
 
-    def is_going_to_state7(self, event):
+    def is_going_to_occupied(self, event):
         text = event.message.text
         return text == "網孔已被註冊"
 
-    def is_going_to_state8(self, event):
+    def is_going_to_change(self, event):
         text = event.message.text
         return text == "紅色叉叉"
 
-    def is_going_to_state9(self, event):
+    def is_going_to_call(self, event):
         text = event.message.text
         return text == "黃色驚嘆號" or text == "仍無法連線" or text == "欲與室友交換網孔" or text == "否"
 
-    def is_going_to_state10(self, event):
+    def is_going_to_find_another(self, event):
         text = event.message.text
         return text == "是"
 
@@ -72,8 +72,8 @@ class TocMachine(GraphMachine):
     def on_exit_start(self, event):
         print("Leaving start")
 
-    def on_enter_state1(self, event):
-        print("I'm entering state1")
+    def on_enter_before_register(self, event):
+        print("I'm entering before_register")
 
         text = ["尚未開放註冊宿網","請選擇連接宿網之方式"] 
         buttons = ["使用分享器", "無使用分享器"]
@@ -81,11 +81,11 @@ class TocMachine(GraphMachine):
         reply_token = event.reply_token
         send_button_message(reply_token, text, buttons)
 
-    def on_exit_state1(self, event):
-        print("Leaving state1")
+    def on_exit_before_register(self, event):
+        print("Leaving before_register")
 
-    def on_enter_state2(self, event):
-        print("I'm entering state2")
+    def on_enter_after_register(self, event):
+        print("I'm entering after_register")
 
         text = ["已開始註冊宿網", "請選擇註冊情況"]
         buttons = ["已註冊", "未註冊", "網孔已被註冊"]
@@ -93,11 +93,11 @@ class TocMachine(GraphMachine):
         reply_token = event.reply_token
         send_button_message(reply_token, text, buttons)
 
-    def on_exit_state2(self, event):
-        print("Leaving state2")
+    def on_exit_after_register(self, event):
+        print("Leaving after_register")
 
-    def on_enter_state3(self, event):
-        print("I'm entering state3")
+    def on_enter_use_router(self, event):
+        print("I'm entering use_router")
 
         text = ["請確認分享器設定如下:", 
                 "1. DHCP(自動取得IP&DNS)\n2. Router mode(通常是預設值)"]
@@ -106,11 +106,11 @@ class TocMachine(GraphMachine):
         reply_token = event.reply_token
         send_button_message(reply_token, text, buttons)
 
-    def on_exit_state3(self, event):
-        print("Leaving state3")
+    def on_exit_use_router(self, event):
+        print("Leaving use_router")
 
-    def on_enter_state4(self, event):
-        print("I'm entering state4")
+    def on_enter_not_use_router(self, event):
+        print("I'm entering not_use_router")
 
         text = ["無使用分享器", "請確認右下角網路連接狀態顯示何種圖示"]
         buttons = ["紅色叉叉", "黃色驚嘆號"]
@@ -118,11 +118,11 @@ class TocMachine(GraphMachine):
         reply_token = event.reply_token
         send_button_message(reply_token, text, buttons)
 
-    def on_exit_state4(self, event):
-        print("Leaving state4")
+    def on_exit_not_use_router(self, event):
+        print("Leaving not_use_router")
 
-    def on_enter_state5(self, event):
-        print("I'm entering state5")
+    def on_enter_already_register(self, event):
+        print("I'm entering already_register")
 
         text = ["已註冊宿網","請確認右下角網路連接狀態顯示何種圖示"] 
         buttons = ["紅色叉叉", "黃色驚嘆號"]
@@ -130,11 +130,11 @@ class TocMachine(GraphMachine):
         reply_token = event.reply_token
         send_button_message(reply_token, text, buttons)
 
-    def on_exit_state5(self, event):
-        print("Leaving state5")
+    def on_exit_already_register(self, event):
+        print("Leaving already_register")
 
-    def on_enter_state6(self, event):
-        print("I'm entering state6")
+    def on_enter_not_register(self, event):
+        print("I'm entering not_register")
 
         text = ["尚未註冊",
                 "請直接使用學校宿網連線進入宿網管理系統，\n並確認無連上個人行動熱點等wifi"] 
@@ -143,11 +143,11 @@ class TocMachine(GraphMachine):
         reply_token = event.reply_token
         send_button_message(reply_token, text, buttons)
 
-    def on_exit_state5(self, event):
-        print("Leaving state5")
+    def on_exit_already_register(self, event):
+        print("Leaving already_register")
 
-    def on_enter_state6a(self, event):
-        print("I'm entering state6a")
+    def on_enter_check_url(self, event):
+        print("I'm entering check_url")
 
         text = ["尚未註冊",
                 "請輸入網址http://dorm.cc.ncku.edu.tw/進行註冊\n注意：開頭是http沒有s！"]
@@ -156,11 +156,11 @@ class TocMachine(GraphMachine):
         reply_token = event.reply_token
         send_button_message(reply_token, text, buttons)
 
-    def on_exit_state6a(self, event):
-        print("Leaving state6a")
+    def on_exit_check_url(self, event):
+        print("Leaving check_url")
 
-    def on_enter_state7(self, event):
-        print("I'm entering state7")
+    def on_enter_occupied(self, event):
+        print("I'm entering occupied")
 
         text = ["網孔已被註冊","請先確認是否為室友註冊"] 
         buttons = ["是", "否"]
@@ -168,11 +168,11 @@ class TocMachine(GraphMachine):
         reply_token = event.reply_token
         send_button_message(reply_token, text, buttons)
 
-    def on_exit_state7(self, event):
-        print("Leaving state7")
+    def on_exit_occupied(self, event):
+        print("Leaving occupied")
 
-    def on_enter_state8(self, event):
-        print("I'm entering state8")
+    def on_enter_change(self, event):
+        print("I'm entering change")
 
         text = ["網路連接狀態顯示紅色叉叉","請更換轉接頭或網路線測試"] 
         buttons = ["連線成功", "仍無法連線"]
@@ -180,11 +180,11 @@ class TocMachine(GraphMachine):
         reply_token = event.reply_token
         send_button_message(reply_token, text, buttons)
 
-    def on_exit_state8(self, event):
-        print("Leaving state8")
+    def on_exit_change(self, event):
+        print("Leaving change")
 
-    def on_enter_state9(self, event):
-        print("I'm entering state9")
+    def on_enter_call(self, event):
+        print("I'm entering call")
 
         text = ["轉由專人服務", "請致電計網中心分機61010詢問，謝謝！"]
         buttons = ["我知道了"]
@@ -192,11 +192,11 @@ class TocMachine(GraphMachine):
         reply_token = event.reply_token
         send_button_message(reply_token, text, buttons)
 
-    def on_exit_state9(self, event):
-        print("Leaving state9")
+    def on_exit_call(self, event):
+        print("Leaving call")
 
-    def on_enter_state10(self, event):
-        print("I'm entering state10")
+    def on_enter_find_another(self, event):
+        print("I'm entering find_another")
 
         text = ["已被室友註冊","請更換至其他尚未被註冊之網孔進行註冊"] 
         buttons = ["已更換至未註冊網孔", "欲與室友交換網孔"]
@@ -204,8 +204,8 @@ class TocMachine(GraphMachine):
         reply_token = event.reply_token
         send_button_message(reply_token, text, buttons)
 
-    def on_exit_state10(self, event):
-        print("Leaving state10")
+    def on_exit_find_another(self, event):
+        print("Leaving find_another")
 
     def on_enter_final(self, event):
         print("I'm entering final")
